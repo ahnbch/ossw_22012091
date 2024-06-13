6a90fc31a95c4826c0d410280f4701a95fb9159b
ahnbch <165380592+ahnbch@users.noreply.github.com>
2024-06-13 16:52:05 +0900

    Delete ossw directory

--- ossw/src/Result.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Result {
-    public int total(List<Info> infoList) {
-        return infoList.stream()
-                .mapToInt(Info::getMoney)
-                .sum();
-    }
-}
